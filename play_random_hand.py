12992e2fcd7706a6fa71e6e08f41c855bce3181c
Valentin <valentin.foucher@free.fr>
2023-09-24 19:06:08 +0200

    feat: update script to play random hands with 2 to 10 players

--- play_random_hand.py
+++ play_random_hand.py
@@ -1,8 +1,8 @@
+import argparse
 import os
 from typing import Optional
 
 from poker_monte_carlo import stack
-from poker_monte_carlo.combinations import find_best_combination, compare_combinations
 from poker_monte_carlo.model import Card
 from poker_monte_carlo.parser import parse_board, parse_hand, parse_stack
 from poker_monte_carlo.simulation import get_percentage_for_hands
@@ -19,51 +19,49 @@ def clear_screen():
             raise Exception(f'OS not recognized ({other}')
 
 
-def play_street(s: list[str], first_player_hand: tuple[Card, ...], second_player_hand: tuple[Card, ...],
-                b: Optional[str] = None):
+def play_street(s: list[str], hands: list[tuple[Card, ...]], b: Optional[str] = None) -> list[float]:
 
     parsed_board = parse_board(b) if b else []
 
-    wins = get_percentage_for_hands(
-        [first_player_hand, second_player_hand],
+    win_percentages = get_percentage_for_hands(
+        hands,
         parse_stack(s),
         parsed_board
     )
 
     clear_screen()
-    print(f'first player hand: {first_player_hand} - {wins[0]}%\n\n'
-          f'{f"Board: {parsed_board}" if parsed_board else ""}\n\n'
-          f'Second player hand: {second_player_hand} - {wins[1]}%\n')
+    scoreboard = ''
+    for i, (hand, percentage) in enumerate(zip(hands, win_percentages)):
+        scoreboard += f'Player {i + 1}: {hand} - {percentage}%\n'
 
-    return parsed_board
+    scoreboard += f'\n{f"Board: {parsed_board}" if parsed_board else ""}\n'
+    print(scoreboard)
+
+    return win_percentages
 
 
-def main():
+def main(player_count: int):
     while True:
         s = stack.generate_stack()
 
-        first_player_hand = parse_hand(stack.draw_hand(s))
-        second_player_hand = parse_hand(stack.draw_hand(s))
-        play_street(s, first_player_hand, second_player_hand)
+        hands = [parse_hand(stack.draw_hand(s)) for _ in range(player_count)]
+        play_street(s, hands)
 
         board = stack.draw_flop(s)
-        play_street(s, first_player_hand, second_player_hand, board)
+        play_street(s, hands, board)
 
         board = stack.draw_turn(s, board)
-        play_street(s, first_player_hand, second_player_hand, board)
+        play_street(s, hands, board)
 
         board = stack.draw_river(s, board)
-        final_board = play_street(s, first_player_hand, second_player_hand, board)
+        win_percentages = play_street(s, hands, board)
 
-        result = compare_combinations(find_best_combination(first_player_hand, final_board),
-                                      find_best_combination(second_player_hand, final_board))
+        winners = []
+        for i, percentage in enumerate(win_percentages):
+            if percentage == 100:
+                winners.append(str(i + 1))
 
-        if result == 1:
-            print('Player 1 wins!')
-        elif result == -1:
-            print('Player 2 wins!')
-        else:
-            print('Draw!')
+        print(f'Player{"s" if len(winners) > 1 else ""} {", ".join(winners)} win{"s" if len(winners) == 1 else ""}!')
 
         user_response = input('\nDo you want to play another hand? (y/n)')
         if user_response not in ('y', 'Y'):
@@ -71,4 +69,13 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Script allowing to play random poker hands and hinting the win odds '
+                                                 'for each player')
+    parser.add_argument(
+        '-n', '--players-count',
+        default=2,
+        type=int,
+        choices=range(2, 11)
+    )
+
+    main(parser.parse_args().players_count)
